0f05df0b7684e7a1214252c7b0376e4a5ce66386
fiorin7 <diana.zulcheva@gmail.com>
2020-06-14 14:07:39 +0300

    chore: improve matcher module readability by adding new variables

--- matcher.py
+++ matcher.py
@@ -1,68 +1,70 @@
 import re
 
-def is_adj_1_2_decl(content0, content1):
-    return content1.strip().isdigit() and ',' not in content0 and content0[-2:].strip() == 'us'
+def is_adj_1_2_decl(content0s, content1s):
+    return content1s.isdigit() and ',' not in content0s and content0s[-2:] == 'us'
 
-def is_adv(content0, content1):
-    return content1.strip() == 'adv.' and len(re.split(', | ', content0.strip())) == 1
+def is_adv(content0s, content1s):
+    return content1s == 'adv.' and len(re.split(', | ', content0s)) == 1
 
-def is_one_form_verb(content0, content1):
-    return content1.strip().isdigit() and content0[-1].strip() == 'o'
+def is_one_form_verb(content0s, content1s):
+    return content1s.isdigit() and content0s[-1] == 'o'
 
-def is_deponent_verb(content0, content1):
-    return content1.strip().isdigit() and (content0.strip()[-1] == 'o' or \
-        (content0.split(', ')[0].strip()[-2:] == 'or' and content0.split(', ')[1].strip()[-3:] == 'sum') or\
-            content0.strip()[-4:] == 'o(r)' or content0.strip()[-2:] == 'or')
+def is_deponent_verb(content0s, content1s):
+    return content1s.isdigit() and (content0s[-1] == 'o' or \
+        (content0s.split(', ')[0].strip()[-2:] == 'or' and content0s.split(', ')[1].strip()[-3:] == 'sum') or\
+            content0s[-4:] == 'o(r)' or content0s[-2:] == 'or')
 
 def is_special_verb(content0):
     first_word = content0.split(', ')[0].strip()
     return first_word[-4:] == '-sum' or first_word[-5:] == '-fero' or first_word[-3:] == '-eo'
 
-def is_simple_noun(content0, content1):
-    return  content1.strip() in ('m', 'f', 'n', 'mf')
+def is_simple_noun(content1s):
+    return  content1s in ('m', 'f', 'n', 'mf')
 
-def is_greek_noun(content0, content1):
-    return  content1.strip() in ('m (гр.)', 'f (гр.)', 'n (гр.)', 'mf (гр.)')
+def is_greek_noun(content1s):
+    return  content1s in ('m (гр.)', 'f (гр.)', 'n (гр.)', 'mf (гр.)')
 
-def is_noun(content0, content1):
-    return any([is_simple_noun(content0, content1),
-    is_greek_noun(content0, content1)])
+def is_noun(content1s):
+    return any([is_simple_noun(content1s),
+    is_greek_noun(content1s)])
 
 # *********************
 
-def is_verb_based_on_conj_number(content0, content1):
-    return  content1.strip() in ('2', '4')
+def is_verb_based_on_conj_number(content1s):
+    return  content1s in ('2', '4')
 
-def is_verb_based_on_conj_number_and_forms_count(content0, content1):
-    return (content1.strip() in ('2', '3', '4')) and len(content0.split(', ')) >= 2
+def is_verb_based_on_conj_number_and_forms_count(content0s, content1s):
+    return (content1s in ('2', '3', '4')) and len(content0s.split(', ')) >= 2
 
 # ********************
 
-def is_conjunct(content0, content1):
-    return content1.strip() in ('coniunct.', 'coniunct') and len(re.split(', | ', content0.strip())) == 1
+def is_conjunct(content0s, content1s):
+    return content1s in ('coniunct.', 'coniunct') and len(re.split(', | ', content0s)) == 1
 
-def is_praep(content0, content1):
-    return content1.strip().split()[0] == 'praep.' and content1.strip().split()[1] == 'с' and len(content1.strip().split()[2]) == 4
+def is_praep(content1s):
+    return content1s.split()[0] == 'praep.' and content1s.split()[1] == 'с' and len(content1s.split()[2]) == 4
 
-def is_multiple_form_verb(content0, content1):
-    return any([is_verb_based_on_conj_number(content0, content1),
-    is_verb_based_on_conj_number_and_forms_count(content0, content1)])
+def is_multiple_form_verb(content0s, content1s):
+    return any([is_verb_based_on_conj_number(content1s),
+    is_verb_based_on_conj_number_and_forms_count(content0s, content1s)])
     
 
 def match_and_prefix_form_and_grammar_meta(contents):
     content0 = contents[0].text
     content1 = contents[1].text
+    content0s = contents[0].text.strip()
+    content1s = contents[1].text.strip()
     
     return any([
-    is_adj_1_2_decl(content0, content1),
-    is_adv(content0, content1),
-    is_one_form_verb(content0, content1),
-    is_multiple_form_verb(content0, content1),
-    is_deponent_verb(content0, content1),
+    is_adj_1_2_decl(content0s, content1s),
+    is_adv(content0s, content1s),
+    is_one_form_verb(content0s, content1s),
+    is_multiple_form_verb(content0s, content1s),
+    is_deponent_verb(content0s, content1s),
     is_special_verb(content0),
-    is_noun(content0, content1),
-    is_conjunct(content0, content1),
-    is_praep(content0, content1)])
+    is_noun(content1s),
+    is_conjunct(content0s, content1s),
+    is_praep(content1s)])
 
 
 
